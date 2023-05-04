c6fd4910835e68fba7b3c9602cfc1b0f3acbef19
Igor Korshunov <igor.9206@gmail.com>
2023-05-04 21:44:39 +0600

    task2 correction

--- src/main/kotlin/task2.kt
+++ src/main/kotlin/task2.kt
@@ -1,11 +1,51 @@
 fun main() {
-    println(transfer(sumTransfer = 1000))
-    println(transfer("Maestro", 75001, 1000))
+    println(transfer(sumMonth = 40000, sumTransfer = 15000))
+    println(transfer("Maestro", 75001, 51000))
     println(transfer("Visa", 5000, 10000))
     println(transfer("UnionPay", 5000, 10000))
 }
 
-fun transfer(typeCard: String = "VK Pay", sumMonth: Int = 0, sumTransfer: Int) : String {
+fun transfer(typeCard: String = "VK Pay", sumMonth: Int = 0, sumTransfer: Int): String {
+
+    val checkLimit = checkLimitTransfer(typeCard, sumMonth, sumTransfer)
+
+    return when (typeCard) {
+        "VK Pay", "MasterCard", "Maestro", "Visa", "Mir" -> if (checkLimit == "OK")
+            calcCommission(typeCard, sumMonth, sumTransfer) else checkLimit
+
+        else -> "Invalid typeCard name"
+    }
+}
+
+fun checkLimitTransfer(typeCard: String, sumMonth: Int, sumTransfer: Int): String {
+    val msgErrorDayLimit = "Превышен дневной лимит переводов"
+    val msgErrorMonthLimit = "Превышен лимит переводов в месяц"
+    val msgErrorAllLimit = "Превышен дневной лимит переводов и в месяц"
+    val vkPayLimitDay = 15_000
+    val vkPayLimitMonth = 40_000
+    val limitDay = 150_000
+    val limitMonth = 600_000
+
+    return when (typeCard) {
+        "VK Pay" -> when {
+            sumTransfer > vkPayLimitDay && sumMonth > vkPayLimitMonth -> msgErrorAllLimit
+            sumTransfer > vkPayLimitDay -> msgErrorDayLimit
+            sumMonth > vkPayLimitMonth -> msgErrorMonthLimit
+            else -> "OK"
+        }
+
+        "MasterCard", "Maestro", "Visa", "Mir" -> when {
+            sumTransfer > limitDay && sumMonth > limitMonth -> msgErrorAllLimit
+            sumTransfer > limitDay -> msgErrorDayLimit
+            sumMonth > limitMonth -> msgErrorMonthLimit
+            else -> "OK"
+        }
+
+        else -> "Invalid typeCard name"
+    }
+}
+
+fun calcCommission(typeCard: String, sumMonth: Int, sumTransfer: Int): String {
     val masterMaestroMonthLimit = 75_000
     val visaMirMinCommissionRub = 35
     val zeroCommission = 0
@@ -17,6 +57,7 @@ fun transfer(typeCard: String = "VK Pay", sumMonth: Int = 0, sumTransfer: Int) :
         "VK Pay" -> "$zeroCommission"
         "MasterCard", "Maestro" ->
             if (sumMonth > masterMaestroMonthLimit) "$commissionMasterMaestro" else "$zeroCommission"
+
         "Visa", "Mir" ->
             if (commissionVisaMir < visaMirMinCommissionRub) "$visaMirMinCommissionRub" else "$commissionVisaMir"
 
